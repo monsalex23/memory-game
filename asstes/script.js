6106080b2e37d4ccb3f2c7739145f39f3d77c79d
monsalex23 <monsalex23@gmail.com>
2022-06-14 11:38:13 -0300

    add function to scripts

--- asstes/script.js
+++ asstes/script.js
@@ -1,10 +1,56 @@
-const cards = document.querySelectorAll ('.card');
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let fristCard, secondCard;
+let lockBoard = false;
 
 function flipCards() {
-    this.classList.toggle('flip');
+    if(lockBoard) return;
+    if(this === fristCard) return;
 
+    this.classList.add('flip');
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        fristCard = this;
+        return;
+    }
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMath();
 }
 
-cards.forEach((cards) =>{
+function checkForMath() {
+    if(fristCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards() {
+    fristCard.removeEventListener('click', flipCards);
+    secondCard.removeEventListener('click', flipCards);
+    
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() =>{
+        fristCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [flipCards, secondCard] = [null, null];
+}
+
+cards.forEach((cards) => {
     cards.addEventListener('click', flipCards);
 })
+
