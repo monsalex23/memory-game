16bb1ea47b763a45bb4f807fd2ee3fcaa81e5cdc
monsalex23 <monsalex23@gmail.com>
2022-06-14 09:42:56 -0300

    memory game js

---
+++ asstes/script.js
@@ -0,0 +1,10 @@
+const cards = document.querySelectorAll ('.card');
+
+function flipCards() {
+    this.classList.toggle('flip');
+
+}
+
+cards.forEach((cards) =>{
+    cards.addEventListener('click', flipCards);
+})
